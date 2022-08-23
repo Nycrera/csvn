7cedbe73721c1a8ee3fbfc4a9c668048a4892aec
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2022-08-23 15:51:47 +0300

    Create Action Poroducer

--- src/main/java/csvn/pubsub/ActionProducer.java
+++ src/main/java/csvn/pubsub/ActionProducer.java
@@ -10,84 +10,49 @@ package csvn.pubsub;
  */
 import core.kafka.connection.IAppConfigs;
 import java.io.File;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Scanner;
 import core.kafka.communication.types.Action;
 import core.models.DateConverter;
+import csvn.Util;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
 import core.kafka.serialization.ObjectSerializer;
 
 public class ActionProducer {
 
     static Logger logger = LogManager.getLogger(StatusProducer.class.getName());
-
-    public static void main(String[] args) throws InterruptedException {
-        KafkaProducer<String, Action> kafkaProducer = new KafkaProducer<String, Action>(getKafkaProducerConfig());
-        Action action = getAction();
-
+    static KafkaProducer<String, Action> kafkaProducer = new KafkaProducer<String, Action>(getKafkaProducerConfig());
+    
+    public static void Send(String ActionName, String Action, Map<String,String> Properties) throws Exception {
+        Action action = new Action();
+        action.setActionName(ActionName);
+        action.setAction(Action);
+        action.setActionProperties((new JSONObject(Properties)).toString());
+        String senderid = Util.DetectIfServer() ? "Server" : Util.DetectOpconUsingIP();
+        action.setSenderId(senderid);
+        action.setActionDate(DateConverter.dateToLong());
         ProducerRecord<String, Action> statusRecord = new ProducerRecord<>(IAppConfigs.ACTION_TOPIC,
                 "action", action);
         kafkaProducer.send(statusRecord);
-
         logger.info("Event published...");
+    }
+    
+    public static void Close() throws InterruptedException {
         kafkaProducer.flush();
         kafkaProducer.close();
-        logger.info("Producer closed...");
         Thread.sleep(2);
-
+        logger.info("Producer closed...");
     }
-
-    private static Action getAction() {
-        Action action = new Action();
-        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-        
-
-        int record_type = 0;
-        int command_type = 0;
-        while (record_type != 1 && record_type != 2) {
-            System.out.println("Lutfen kayit tipi seciniz :\n1- Konsol Kayıt\n2- Monitör Kayıt\n");
-            record_type = scanner.nextInt();
-        }
-
-        while (command_type != 1 && command_type != 2 && command_type != 3) {
-            System.out.println("Lutfen islemi seciniz :\n1- Kayit Baslat\n2- Kayit Durdur\n3- Kayit Kaydet\n");
-            command_type = scanner.nextInt();
-        }
-
-        if (record_type == 1) {
-
-            action.setActionName("console");
-        } else if (record_type == 2) {
-
-            action.setActionName("display");
-        }
-
-        if (command_type == 1) {
-            action.setAction("start");
-
-        } else if (command_type == 2) {
-
-            action.setAction("stop");
-        } else if (command_type == 3) {
-            action.setAction("save");
-
-        }
-        System.out.println("Kayit özelliklerinizi giriniz : \n");
-        String action_properties;
-        scanner.nextLine();
-        action_properties = scanner.nextLine();
-        action.setSenderId(IAppConfigs.SENDER_ID);
-        action.setActionDate(DateConverter.dateToLong());
-        action.setActionProperties(action_properties);
-
-        return action;
-    }
-
+    
     private static Properties getKafkaProducerConfig() {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, IAppConfigs.BOOTSTAP_SERVER);
